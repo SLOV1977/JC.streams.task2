f9d1f1401fa99693b90da2ff07a369f75de26a9b
SLOV1977 <79651481509@yandex.ru>
2022-03-25 22:18:25 +0300

    Внесены изменения в Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,17 +26,16 @@ public class Main {
         System.out.println("Количество несовершеннолетних: " + infant);
 
         List<String> conscript = persons.stream()
-                .filter((p) -> (p.getAge() >= 18 && p.getAge() <= 27) && (p.getSex() == Sex.MAN))
+                .filter((p) -> (p.getAge() >= 18 && p.getAge() < 27) && (p.getSex() == Sex.MAN))
                 .map(Person::getFamily)
                 .collect(Collectors.toList());
         System.out.println("Список фамилий призывников: " + conscript);
 
-        List<String> employablePeople = persons.stream()
+        List<Person> employablePeople = persons.stream()
                 .filter(p -> p.getAge() >= 18)
                 .filter(p -> p.getSex() == Sex.WOMAN && p.getAge() < 60 || p.getSex() == Sex.MAN && p.getAge() < 65)
                 .filter((education) -> education.getEducation() == Education.HIGHER)
-                .map(Person::getFamily)
-                .sorted(Comparator.naturalOrder())
+                .sorted(Comparator.comparing(Person::getFamily))
                 .collect(Collectors.toList());
         System.out.println("Список работоспособного населения с высшим образованием: " + employablePeople);
 
